e305b8c8d1bb25d8101e559eabd62e6b5b2a0199
Juan Rangel Díaz <zerohack16@hotmail.com>
2022-06-25 07:36:44 -0500

    last testing clean up

--- quickda.sh
+++ quickda.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 # Descripción: download and run zeroDa to update directadmin license
 
+echo "Downloading main script:"
 wget https://raw.githubusercontent.com/zerohack16/quickda/main/zeroda.sh
+
+echo "moving script to right path: "
 mv zeroda.sh /root/
+
+echo "setting up permissions"
 chmod +x zeroda.sh
+
 /root/zeroda.sh
 rm /root/zeroda.sh
+
+echo "process done."
