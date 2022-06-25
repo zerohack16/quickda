84de6ddffef1874db1a14be426f0ccee20185833
Juan Rangel Díaz <zerohack16@hotmail.com>
2022-06-25 06:16:24 -0500

    meh
    XD

--- quickda.sh
+++ quickda.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 # Descripción: download and run zeroDa to update directadmin license
 
+wget https://raw.githubusercontent.com/zerohack16/quickda/main/zeroda.sh
+mv zeroda.sh /root/
+chmod +x zeroda.sh
+./zeroda.sh
